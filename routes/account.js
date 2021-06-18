a5194b0480ee50662bf2ec8cb9b94613a64eab2d
Steven Chen <schen196@my.centennialcollege.ca>
2021-06-17 20:05:59 -0400

    fixed account.js routing issue

--- routes/account.js
+++ routes/account.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
@@ -19,23 +18,3 @@ router.get('/', (req, res, next) => {
 });
 
 module.exports = router;
-=======
-let express = require('express');
-let router = express.Router();
-let mongoose = require('mongoose');
-
-// connect to our Account Model
-let Account = require('../models/account');
-
-/* GET Route for Account List page - READ Operation */
-router.get('/', (req, res, next) => {
-    Account.find((err, AccountList) => {
-        if(err){
-            return console.error(err);
-        }
-        else{
-            console.log(AccountList);
-        }
-    });
-});
->>>>>>> 67d2dbad5739f6658fbf433a76a38630af7acead
