ac9caef57b2786c47452a34c3f28af66d95674ce
Steven Chen <schen196@my.centennialcollege.ca>
2021-06-17 23:42:36 -0400

    removed comments

--- config/db.js
+++ config/db.js
@@ -2,25 +2,3 @@ module.exports =
 {
 	"URI": "mongodb://localhost/accounts"
 }
-
-// var mongoose = require('mongoose');
-
-// mongoose.Promise = global.Promise;
-// mongoose.connect("mongodb://localhost:27017/authenticationDB",
-// {useNewUrlParser: true,  useUnifiedTopology: true });
-
-// const accountSchema = new mongoose.Schema({
-// 	username: String,
-// 	password: String,
-// 	email: String
-//    });
-
-// const user = mongoose.model("user", accountSchema);
-
-// const account = new user({
-// 	username: "steven",
-// 	password: "test",
-// 	email: "test@email.com"
-//    });
-
-// account.save();
