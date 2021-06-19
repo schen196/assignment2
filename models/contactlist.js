8dc9532fd138baa5b4b1f6642a26bd1e403d02ce
Steven Chen <schen192@my.centennialcollege.ca>
2021-06-18 22:42:32 -0400

    updated contact model

--- models/contactlist.js
+++ models/contactlist.js
@@ -3,32 +3,9 @@ let mongoose = require('mongoose');
 // create a model class
 
 let contactModel = mongoose.Schema({
-    contactName:{
-        type: String,
-        default: "",
-        trim: true,
-        required: "Contact Name is required"
-    },
- 	contactNumber:{
-        type: Int32Array,
-        default: "",
-        trim: true,
-        required: "Contact Number is required"
-     },
- 	email:{
-        type: String,
-        default: '',
-        trim: true,
-        required: "Email address is required."
-     },
-     created:{
-         type: Date,
-         default: Date.now
-     },
-     update:{
-         type: Date,
-         default: Date.now
-     }
+    name: String,
+    number: Int32Array,
+    email: String
 },
 {
     collection: "contacts"
