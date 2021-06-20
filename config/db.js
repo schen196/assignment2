1105182327dc0321a42015aeca7d4257b4dfcd1c
Steven Chen <schen196@my.centennialcollege.ca>
2021-06-20 01:12:15 -0400

    installed dotenv, and connected to mongoDB atlas

--- config/db.js
+++ config/db.js
@@ -1,14 +1,15 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
 
 const connectToMongoDB = () => {
-	const URI = "mongodb://localhost/accounts";
+	const URI = process.env.URI;
 	const options = {
 		useNewUrlParser: true,
 		useUnifiedTopology: true
 	};
 	mongoose.connect(URI, options)
 		.then(() => console.log("Connected to MongoDB"))
-		.catch(error => console.log("Connection Error:", error));
+		.catch(error => console.log("Connection Error:", error));	
 };
 
 module.exports = connectToMongoDB;
